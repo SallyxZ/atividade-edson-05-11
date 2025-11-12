62d68cb70a815002ddc936d05bdb83a9a8eb5f0b
MURILO PEREIRA <00120375@unisoacad.uniso.br>
2025-11-12 20:11:16 -0300

    API 12 11

---
+++ api/atividade_WebAPI/atividade_WebAPI/Controllers/PlayerController.cs
@@ -0,0 +1,70 @@
+﻿using atividade_WebAPI.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace atividade_WebAPI.Controllers
+{
+    public class PlayerController : ControllerBase
+    {
+        public static List<Player> players = new List<Player>() {
+            { new Player { id = "1", Vida = 3, QuantidadeItens = 0, PosicaoX = 0f, PosicaoY = 0f, PosicaoZ = 0f } },
+            { new Player { id = "2", Vida = 2, QuantidadeItens = 1, PosicaoX = 3.5f, PosicaoY = 1.2f, PosicaoZ = 2f } }
+        };
+
+        [HttpGet]
+        [Route("Player")]
+        public IActionResult GetPlayers()
+        {
+            return Ok(players);
+        }
+
+        [HttpGet]
+        [Route("Player/{id}")]
+        public IActionResult GetPlayerByID(string id)
+        {
+            var player = players.FirstOrDefault(a => a.id == id);
+            if (player == null)
+            {
+                return NotFound();
+            }
+            return Ok(player);
+        }
+
+        [HttpPost]
+        [Route("Player")]
+        public IActionResult AddPlayer([FromBody] Player newPlayer)
+        {
+            players.Add(newPlayer);
+            return Ok(newPlayer);
+        }
+
+        [HttpPut]
+        [Route("Player/{id}")]
+        public IActionResult UpdatePlayer(string id, [FromBody] Player playerUpdated)
+        {
+            var player = players.FirstOrDefault(a => a.id == id);
+            if (player == null)
+            {
+                return NotFound();
+            }
+            player.Vida = playerUpdated.Vida;
+            player.QuantidadeItens = playerUpdated.QuantidadeItens;
+            player.PosicaoX = playerUpdated.PosicaoX;
+            player.PosicaoY = playerUpdated.PosicaoY;
+            player.PosicaoZ = playerUpdated.PosicaoZ;
+            return Ok(player);
+        }
+
+        [HttpDelete]
+        [Route("Player/{id}")]
+        public IActionResult DeletePlayer(string id)
+        {
+            var player = players.FirstOrDefault(a => a.id == id);
+            if (player == null)
+            {
+                return NotFound();
+            }
+            players.Remove(player);
+            return Ok();
+        }
+    }
+}
---
+++ api/atividade_WebAPI/atividade_WebAPI/Model/Player.cs
@@ -0,0 +1,12 @@
+﻿namespace atividade_WebAPI.Model
+{
+    public class Player
+    {
+        public string id { get; set; }
+        public int Vida { get; set; }
+        public int QuantidadeItens { get; set; }
+        public float PosicaoX { get; set; }
+        public float PosicaoY { get; set; }
+        public float PosicaoZ { get; set; }
+    }
+}
